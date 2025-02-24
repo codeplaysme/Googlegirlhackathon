228005462e48c15d3b9aa235d6724a38d2013919
Vaishnavi Kulkarni <vaishnavi.kulkarni6306@gmail.com>
2025-02-24 20:17:00 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,22 @@
+import Header from "./components/Header"
+import Hero from "./components/Hero"
+import Features from "./components/Features"
+import ProcessExamples from "./components/ProcessExamples"
+import ContactForm from "./components/ContactForm"
+import Footer from "./components/Footer"
+
+function App() {
+  return (
+    <div className="font-sans">
+      <Header />
+      <Hero />
+      <Features />
+      <ProcessExamples />
+      <ContactForm />
+      <Footer />
+    </div>
+  )
+}
+
+export default App
+
